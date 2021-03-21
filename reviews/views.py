2aed1b5301d4a247d9c1dbfca519f195632570dc
cheenuthor <venkatakrishnanmdu@gmail.com>
2021-03-21 14:40:55 +0530

    Using FormView instead of VIew

--- reviews/views.py
+++ reviews/views.py
@@ -5,25 +5,19 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import FormView
 # from .models import ReviewModel
 # Create your views here.
 
 
-class ReviewView(View):
-    def get(self, request):
-        form = ReviewForm()
-        return render(request, "reviews/review.html",  {
-            "form": form
-        })
+class ReviewView(FormView):
+    form_class = ReviewForm
+    template_name = 'reviews/review.html'
+    success_url = "/thank-you"
 
-    def post(self, request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect("/thank-you")
-        return render(request, "reviews/review.html",  {
-            "form": form
-        })
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
 class ThankYouView(TemplateView):
